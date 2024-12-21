c7cdecd310a0b4ed628e0dc1d9a83dfdf6d8f536
HASSAN_ACHEHRRI <hachehri@gmail.com>
2024-12-21 17:42:46 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,14 @@
+print("welcome to the YouTube channel name generator:")
+
+# اكتب اسم لقناتك على اليوتيوب
+name_channel=input("what is your nickname?")
+
+# المحتوى الذي تريد تقديمه
+about=input("what is your channel about?")
+
+# اخترنا لك الاسم لتسميه قناتك على اليوتيوب
+print(f"You could channel name your channel ({name_channel} with {about})")
+
+
+
+
